542d147bd1bba7cd73c5828e9bbd8f44d7033132
trachchung <nguyentrachchung@gmail.com>
2024-10-18 10:43:50 +0700

    nodejs runtime file uploader

---
+++ app/api/upload/route.tsx
@@ -0,0 +1,113 @@
+import {
+  FleekSdk,
+  PersonalAccessTokenService,
+  UploadProgress,
+} from "@fleek-platform/sdk/node";
+import { NextResponse } from "next/server";
+
+export const runtime = "nodejs";
+
+export async function POST(req: Request) {
+  try {
+    const formData = await req.formData();
+    const file = formData.get("file");
+
+    if (!file) {
+      return NextResponse.json({ error: "No file provided" }, { status: 400 });
+    }
+
+    // Logics here
+    const accessTokenService = new PersonalAccessTokenService({
+      personalAccessToken: "pat_weeVqAEs952IyFMW5ysm",
+      projectId: "cm2css5d1000tgqiqz5wd9iaa",
+    });
+    const fleekSdk = new FleekSdk({
+      accessTokenService: accessTokenService,
+    });
+
+    const uploadResult = await fleekSdk.storage().uploadFile({
+      file: file,
+      onUploadProgress: onUploadProgress,
+    });
+
+    console.log("uploadResult", uploadResult);
+    console.log("File uploaded successfully!");
+    return NextResponse.json({ message: "File uploaded successfully!" });
+  } catch (error) {
+    console.error("Error uploading file to IPFS:", error); // Log the error
+    return NextResponse.json(
+      { error: "Error uploading file to IPFS" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req: Request) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const cid = searchParams.get("cid");
+
+    if (!cid) {
+      return NextResponse.json({ error: "No CID provided" }, { status: 400 });
+    }
+
+    const staticProps = await getStaticProps();
+
+    if (!staticProps.props.pat || !staticProps.props.projectId) {
+      return NextResponse.json(
+        { error: "No PAT or Project ID provided" },
+        { status: 400 }
+      );
+    }
+
+    // Logics here
+    const accessTokenService = new PersonalAccessTokenService({
+      personalAccessToken: staticProps.props.pat,
+      projectId: staticProps.props.projectId,
+    });
+
+    const fleekSdk = new FleekSdk({
+      accessTokenService: accessTokenService,
+    });
+
+    const storagePin = await fleekSdk.storage().get({
+      cid: cid,
+    });
+    return NextResponse.json({
+      message: "File get successfully!",
+      data: storagePin,
+    });
+  } catch (error) {
+    console.error("Error uploading file to IPFS:", error); // Log the error
+    return NextResponse.json(
+      { error: "Error uploading file to IPFS" },
+      { status: 500 }
+    );
+  }
+}
+
+const onUploadProgress = ({ loadedSize, totalSize }: UploadProgress) => {
+  if (loadedSize > 0) {
+    console.log("loaded size:", loadedSize);
+    console.log("total size:", totalSize);
+  }
+};
+
+async function getStaticProps() {
+  try {
+    return {
+      props: {
+        pat: process.env.FLEEK_TOKEN,
+        projectId: process.env.PROJECT_ID,
+      },
+    };
+  } catch (error) {
+    console.error("Error getting static props:", error); // Log the error
+    return {
+      props: {
+        pat: "",
+        projectId: "",
+      },
+    };
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,44 +6,113 @@ import { useState } from "react";
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export default function HomePage() {
   const [file, setFile] = useState(null);
-  const [preview, setPreview] = useState("");
+  const [cid, setCid] = useState<string>("");
+  const [uploadPreview, setUploadPreview] = useState("");
+  const [getImagePreview, setGetImagePreview] = useState("");
 
   const handleFileChange = (e: any) => {
     const selectedFile = e.target.files[0];
     setFile(selectedFile);
-    setPreview(URL.createObjectURL(selectedFile));
+    setUploadPreview(URL.createObjectURL(selectedFile));
   };
 
   const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    if (!file) return;
+    try {
+      e.preventDefault();
+      if (!file) return;
 
-    const formData = new FormData();
-    formData.append("file", file);
-    console.log("File:", file);
-    const res = await fetch("/api/upload", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: formData,
-    });
+      const formData = new FormData();
+      formData.append("file", file);
+      const res = await fetch("/api/upload", {
+        method: "POST",
+        body: formData,
+      });
 
-    if (res.ok) {
-      console.log("File uploaded successfully!");
-    } else {
-      console.log("Error uploading file.");
+      if (res.ok) {
+        console.log("File uploaded successfully!");
+      } else {
+        console.log("Error uploading file.");
+      }
+    } catch (error) {
+      console.log("Error uploading file.", error);
+    }
+  };
+
+  const handleFetchImage = async () => {
+    try {
+      if (!cid) return;
+      const ipfsRes = await fetch(`https://gateway.pinata.cloud/ipfs/${cid}`);
+      if (ipfsRes.ok) {
+        const blob = await ipfsRes.blob();
+        const imageUrl = URL.createObjectURL(blob);
+        setGetImagePreview(imageUrl);
+      } else {
+        console.log("Error fetching image.");
+      }
+    } catch (error) {
+      console.log("Error fetching image.", error);
     }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="file" onChange={handleFileChange} />
-        <button type="submit">Upload</button>
+    <div className="flex flex-col items-center justify-center p-6 border border-gray-300 rounded-lg max-w-lg mx-auto bg-white shadow-lg">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center space-y-4"
+      >
+        <label
+          htmlFor="file-upload"
+          className="cursor-pointer bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg transition-colors duration-300"
+        >
+          Choose a file
+        </label>
+        <input
+          id="file-upload"
+          type="file"
+          onChange={handleFileChange}
+          className="hidden"
+        />
+        <button
+          type="submit"
+          className="bg-green-500 hover:bg-green-600 text-white py-2 px-6 rounded-lg transition-colors duration-300"
+        >
+          Upload
+        </button>
+        <input
+          type="text"
+          value={cid}
+          onChange={(e) => setCid(e.target.value)}
+          placeholder="Enter CID hash"
+          className="mb-4 p-2 border border-gray-300 rounded"
+        />
+        <button
+          type="button"
+          onClick={handleFetchImage}
+          className="ursor-pointer  hover:bg-blue-600 text-white py-2 px-4 rounded-lg transition-colors duration-300 mb-4 p-2 bg-blue-500"
+        >
+          Fetch Image
+        </button>
       </form>
-      {preview && (
-        <Image src={preview} alt="Image Preview" width="200" height="200" />
+
+      {uploadPreview && (
+        <div className="mt-6 relative h-[250px] w-full">
+          <Image
+            src={uploadPreview}
+            alt="Upload Image Preview"
+            className="w-48 h-48 object-cover rounded-lg shadow-md"
+            fill
+          />
+        </div>
+      )}
+      {getImagePreview && (
+        <div className="mt-6 relative h-[250px] w-full">
+          <Image
+            src={getImagePreview}
+            alt="Upload Image Preview"
+            className="w-48 h-48 object-cover rounded-lg shadow-md"
+            fill
+          />
+        </div>
       )}
     </div>
   );
